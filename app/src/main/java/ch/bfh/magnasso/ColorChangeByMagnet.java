687b0a601f787bde8101bf6b87b2de80bc21f49d
andrejica <andrejic.aleks91@gmail.com>
2020-03-01 17:43:46 +0100

    Mainactivty test to change color, added new empty activity for the magnet test later

---
+++ app/src/main/java/ch/bfh/magnasso/ColorChangeByMagnet.java
@@ -0,0 +1,14 @@
+package ch.bfh.magnasso;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ColorChangeByMagnet extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_color_change_by_magnet);
+    }
+}
--- app/src/main/java/ch/bfh/magnasso/MainActivity.java
+++ app/src/main/java/ch/bfh/magnasso/MainActivity.java
@@ -1,8 +1,12 @@
 package ch.bfh.magnasso;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final ConstraintLayout layout = findViewById(R.id.mainLayout);
+        final Button redButton = findViewById(R.id.button_turn_red);
+        final Button resetButton = findViewById(R.id.button_reset);
+
+        redButton.setOnClickListener(new View.OnClickListener(){
+
+            public void onClick(View v){
+                //v.setBackgroundColor(Color.RED);
+                layout.setBackgroundColor(Color.RED);
+            }
+        });
+
+        resetButton.setOnClickListener(new View.OnClickListener(){
+
+            public void onClick(View v){
+                //v.setBackgroundColor(Color.WHITE);
+                layout.setBackgroundColor(Color.WHITE);
+            }
+        });
+
+
+
     }
+
+
 }
