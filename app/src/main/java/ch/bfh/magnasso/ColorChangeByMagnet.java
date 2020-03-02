5e3636e9834327e779132630a39e8f89c4d9cd6d
andrejica <andrejic.aleks91@gmail.com>
2020-03-02 20:58:28 +0100

    Displaycolor can be changed with magnet. Not many colors at the moment

--- app/src/main/java/ch/bfh/magnasso/ColorChangeByMagnet.java
+++ app/src/main/java/ch/bfh/magnasso/ColorChangeByMagnet.java
@@ -1,14 +1,128 @@
 package ch.bfh.magnasso;
 
+import android.app.Activity;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.util.Log;
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
-public class ColorChangeByMagnet extends AppCompatActivity {
+public class ColorChangeByMagnet extends Activity implements SensorEventListener {
+
+    private static final String TAG = "Magnet";
+    // Sensors & SensorManager
+    private Sensor magnetometer;
+    private SensorManager mSensorManager;
+
+    // Storage for Sensor readings
+    private float[] mGeomagnetic = null;
+
+    private ConstraintLayout layout = null;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+//        Intent intent = getIntent();
+//        String value = intent.getStringExtra("key");
+        //Log.println(1, "Intent value",value);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_color_change_by_magnet);
+
+        //Layout to change the background from the main activity
+        layout = findViewById(R.id.Magnasso);
+
+        // Get a reference to the SensorManager
+        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+
+        // Get a reference to the magnetometer
+        magnetometer = mSensorManager
+                .getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+
+        // Exit unless sensor are available
+        if (null == magnetometer)
+            finish();
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // Register for sensor updates
+
+        mSensorManager.registerListener(this, magnetometer,
+                SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        // Unregister all sensors
+        mSensorManager.unregisterListener(this);
+
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+        // Acquire magnetometer event data
+
+        if (event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) {
+
+            mGeomagnetic = new float[3];
+            System.arraycopy(event.values, 0, mGeomagnetic, 0, 3);
+
+        }
+
+        // If we have readings from both sensors then
+        // use the readings to compute the device's orientation
+        // and then update the display.
+
+        if (mGeomagnetic != null) {
+
+            int R = (int) mGeomagnetic[0];
+            int G = (int) mGeomagnetic[1];
+            int B = (int) mGeomagnetic[2];
+
+            setBackgroundColorDisplay(R, G, B);
+            Log.d(TAG, "mx : "+mGeomagnetic[0]+" my : "+mGeomagnetic[1]+" mz : "+mGeomagnetic[2]);
+
+        }
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        // N/A
+    }
+
+
+    //Changing MainActivity backgroundcolor with boundaries for the RGB colors not falling below 0
+    private void setBackgroundColorDisplay(int colorR, int colorG, int colorB){
+
+        int red = colorR;
+        int green = colorG;
+        int blue = colorB;
+
+        if(colorR < 0){
+            red = 0;
+        }
+
+        if (colorG < 0){
+            green = 0;
+        }
+
+        if (colorB < 0){
+            blue = 0;
+        }
+
+        int magnassoColor = (255  & 0xff) << 24 | (red & 0xff) << 16 | (green & 0xff) << 8 | (blue & 0xff);
+        layout.setBackgroundColor(magnassoColor);
     }
 }
--- app/src/main/java/ch/bfh/magnasso/MainActivity.java
+++ app/src/main/java/ch/bfh/magnasso/MainActivity.java
@@ -1,12 +1,10 @@
 package ch.bfh.magnasso;
 
-import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,15 +13,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final ConstraintLayout layout = findViewById(R.id.mainLayout);
+        //final ConstraintLayout layout = findViewById(R.id.mainLayout);
         final Button redButton = findViewById(R.id.button_turn_red);
         final Button resetButton = findViewById(R.id.button_reset);
 
         redButton.setOnClickListener(new View.OnClickListener(){
 
             public void onClick(View v){
+//                Intent myIntent = new Intent(MainActivity.this, ColorChangeByMagnet.class);
+//                myIntent.putExtra("key", "You Got the Intent"); //Optional parameters
+//                MainActivity.this.startActivity(myIntent);
                 //v.setBackgroundColor(Color.RED);
-                layout.setBackgroundColor(Color.RED);
+                //layout.setBackgroundColor(Color.RED);
             }
         });
 
@@ -31,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
 
             public void onClick(View v){
                 //v.setBackgroundColor(Color.WHITE);
-                layout.setBackgroundColor(Color.WHITE);
+                //layout.setBackgroundColor(Color.WHITE);
             }
         });
 
